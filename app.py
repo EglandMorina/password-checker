66cde880ff46ddd3e6e41d021bd75572fb1a7a3d
Egland Morina <eglandmorina31@gmail.com>
2026-04-06 13:46:34 -0700

    fix render

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-
+import OS
 app = Flask(__name__)
 
 @app.route("/")
@@ -50,4 +50,5 @@ def checker():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0",
+port=int(os.environ.get("PORT", 10000)))
